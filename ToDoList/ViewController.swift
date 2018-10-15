45f43b97a73567d1a9a1c33344b4e188d17eaed1
Melanie MacDonald <macdon6m@udml7.local>
2018-10-15 14:39:49 -0600

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Melanie MacDonald on 2018-10-15.
+//  Copyright © 2018 Melanie MacDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
